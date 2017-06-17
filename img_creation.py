db07a7c81ebfe93fa38913c3b03fa81b2ce0c6d0
robrao <rao.robby@gmail.com>
2017-06-17 16:51:31 -0600

    fixed indentation

--- img_creation.py
+++ img_creation.py
@@ -119,7 +119,7 @@ width, height = img.size
 xshift = abs(m) * width
 new_width = width + int(round(xshift))
 img = img.transform((new_width, height), Image.AFFINE,
-                (1, m, -xshift if m > 0 else 0, 0, 1, 0), Image.BICUBIC)
+                    (1, m, -xshift if m > 0 else 0, 0, 1, 0), Image.BICUBIC)
 
 for lbl in labels:
     print lbl
