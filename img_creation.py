a68ecab5ca3ba4b1348bcb5227be7df642f0c8ed
robrao <rao.robby@gmail.com>
2017-08-29 20:25:52 -0600

    add if name main

--- img_creation.py
+++ img_creation.py
@@ -43,79 +43,79 @@ def pixel_search(img, x_coord, y_coord, height, max_val):
         y_coord += 0.1
     return x_coord
 
-fonts = []
-for root, dirnames, filenames in os.walk('./fonts/fonts-master/'):
-    for filename in fnmatch.filter(filenames, '*.ttf'):
-        fonts.append(os.path.join(root, filename))
-
-for count, font_path in enumerate([fonts[0]]):
-    font_size = 55
-    num_chars = 26
-    chars = deque(maxlen=num_chars)
-    rnd_black = (0,0,0,0)
-
-    chars_num = []
-    for i in range(0, num_chars):
-    # for i in [5, 6, 7]:
-        c = chars_lower[i]
-        chars.append(c)
-
-    word = "".join(chars)
-
-    font = ImageFont.truetype(font_path, size=font_size)
-
-    # Image size changes in relation to font size
-    im_w_f = font_size * num_chars
-    im_h_f = font_size + im_w_f
-    im_w = int(im_w_f)
-    im_h = int(im_h_f)
-
-    # randomize background on image
-    img = Image.new('RGBA', (im_w, im_h), 'white')
-    draw = ImageDraw.Draw(img, "RGBA")
-    dw, dh = draw.textsize(word, font)
-
-    # per char size
-    char_size = []
-    char_offset = []
-    for c in chars:
-        w, h = draw.textsize(c, font)
-        csize = (w, h)
-        offset = font.getoffset(c)
-        char_size.append(csize)
-        char_offset.append(offset)
-
-    cent_w_img = (im_w - dw) / 2.0
-    cent_h_img = (im_h - dh) / 2.0
-    x_jitter = 0
-    y_jitter = 0
-    x_txt = cent_w_img + x_jitter
-    y_txt = cent_h_img + y_jitter
-
-    draw.text((x_txt, y_txt), word, rnd_black, font=font)
-
-    offset = [x_txt, y_txt]
-    for idx, bbx in enumerate(char_size):
-        charoffset_x, charoffset_y = char_offset[idx]
-        x1 = offset[0] + charoffset_x
-        y1 = offset[1] + charoffset_y
-
-        max_val = max_value_search(img, x1, y1, rnd_black, bbx[0])
-        x1 = pixel_search(img, x1, y1, bbx[1], max_val)
-
-        x2 = bbx[0] + x1
-        y2 = bbx[1] + offset[1]
-        w = bbx[0]/im_w_f
-        h = bbx[1]/im_h_f
-        cx = (x1 + 0.5 * bbx[0])/im_w_f
-        cy = (y1 + 0.5 * bbx[1])/im_h_f
-
-        draw.rectangle([x1, y1, x2, y2], outline='red')
-
-        offset[0] = x2  # THIS IS THE BUG!!!!!!!!
-
-    font_name = os.path.basename(font_path).split(".")[0]
-    title = "{}_{}".format(font_name, count)
-    print title
-    img.show()
-    # dl_pic = raw_input("Delete {}? [y/N]: ".format(font_path))
+if __name__ == "__main__":
+    fonts = []
+    for root, dirnames, filenames in os.walk('./fonts/fonts-master/'):
+        for filename in fnmatch.filter(filenames, '*.ttf'):
+            fonts.append(os.path.join(root, filename))
+    
+    for count, font_path in enumerate([fonts[1]]):
+        font_size = 55
+        num_chars = 26
+        chars = deque(maxlen=num_chars)
+        rnd_black = (0,0,0,0)
+    
+        chars_num = []
+        for i in range(0, num_chars):
+            c = chars_lower[i]
+            chars.append(c)
+    
+        word = "".join(chars)
+    
+        font = ImageFont.truetype(font_path, size=font_size)
+    
+        # Image size changes in relation to font size
+        im_w_f = font_size * num_chars
+        im_h_f = font_size + im_w_f
+        im_w = int(im_w_f)
+        im_h = int(im_h_f)
+    
+        # randomize background on image
+        img = Image.new('RGBA', (im_w, im_h), 'white')
+        draw = ImageDraw.Draw(img, "RGBA")
+        dw, dh = draw.textsize(word, font)
+    
+        # per char size
+        char_size = []
+        char_offset = []
+        for c in chars:
+            w, h = draw.textsize(c, font)
+            csize = (w, h)
+            offset = font.getoffset(c)
+            char_size.append(csize)
+            char_offset.append(offset)
+    
+        cent_w_img = (im_w - dw) / 2.0
+        cent_h_img = (im_h - dh) / 2.0
+        x_jitter = 0
+        y_jitter = 0
+        x_txt = cent_w_img + x_jitter
+        y_txt = cent_h_img + y_jitter
+    
+        draw.text((x_txt, y_txt), word, rnd_black, font=font)
+    
+        offset = [x_txt, y_txt]
+        for idx, bbx in enumerate(char_size):
+            charoffset_x, charoffset_y = char_offset[idx]
+            x1 = offset[0] + charoffset_x
+            y1 = offset[1] + charoffset_y
+    
+            max_val = max_value_search(img, x1, y1, rnd_black, bbx[0])
+            x1 = pixel_search(img, x1, y1, bbx[1], max_val)
+    
+            x2 = bbx[0] + x1
+            y2 = bbx[1] + offset[1]
+            w = bbx[0]/im_w_f
+            h = bbx[1]/im_h_f
+            cx = (x1 + 0.5 * bbx[0])/im_w_f
+            cy = (y1 + 0.5 * bbx[1])/im_h_f
+    
+            draw.rectangle([x1, y1, x2, y2], outline='red')
+    
+            offset[0] = x2  # THIS IS THE BUG!!!!!!!!
+    
+        font_name = os.path.basename(font_path).split(".")[0]
+        title = "{}_{}".format(font_name, count)
+        print title
+        img.show()
+        dl_pic = raw_input("Delete {}? [y/N]: ".format(font_path))
