d387a171d00ab194e48f85dce89dbdc0776f860d
robrao <rob.rao@arterys.com>
2018-04-29 18:33:07 -0600

    catch error when text width larger than image width and continue (ignore), add save point to font_groomer, add pick up where left off check to font groomer

--- font_groomer.py
+++ font_groomer.py
@@ -15,6 +15,20 @@ from random import uniform as rnd_uniform
 
 import numpy as np
 
+
+def cast_to_int(var):
+    try:
+        if var == '0':
+            res = 1
+        else:
+            res = int(var)
+    except Exception as e:
+        res = 0
+    return res
+
+def csv_exists():
+    return os.path.isfile('./font_ratings.csv')
+
 def write_ratings_to_file(font_paths):
     with open("font_ratings.csv", "wb") as outcsv:
         writer = csv.writer(outcsv, delimiter=',', quoting=csv.QUOTE_MINIMAL)
@@ -34,7 +48,19 @@ if __name__ == "__main__":
             fonts.append(os.path.join('/home/rrao/.fonts', ffile))
 
     fonts_ratings = []
+    if csv_exists():
+        with open('font_ratings.csv', 'r') as csvfile:
+            creader = csv.reader(csvfile, delimiter=',')
+            for row in creader:
+                if row[0] == 'font' and row[1] == 'rating':
+                    continue
+                fonts_ratings.append((row[0], row[1]))
+
     for idx, font_path in enumerate(fonts):
+        if idx < len(fonts_ratings) and fonts_ratings[idx][0] == font_path:
+            print "{} -- {}".format(font_path, fonts_ratings[idx][1])
+            continue
+
         font_size = 100
         char_lower = []
         char_upper = []
@@ -50,7 +76,7 @@ if __name__ == "__main__":
         font = ImageFont.truetype(font_path, size=font_size)
 
         # Image size changes in relation to font size
-        im_w_f = font_size * 1.5
+        im_w_f = font_size * 2
         im_h_f = font_size + im_w_f
         im_w = int(im_w_f)
         im_h = int(im_h_f)
@@ -68,12 +94,16 @@ if __name__ == "__main__":
             fonts_ratings.append((font_path, 0))
             continue
 
-        cent_w = (im_w - dw) / 2.0
-        cent_h = (im_h - dh) / 2.0
-        x_jitter = randint(0, int((im_w - dw) * 0.25)) * choice([-1, 1])
-        y_jitter = randint(0, int((im_h - dh) * 0.25)) * choice([-1, 1])
-        txtx = cent_w + x_jitter
-        txty = cent_h + y_jitter
+        try:
+            cent_w = (im_w - dw) / 2.0
+            cent_h = (im_h - dh) / 2.0
+            x_jitter = randint(0, int((im_w - dw) * 0.25)) * choice([-1, 1])
+            y_jitter = randint(0, int((im_h - dh) * 0.25)) * choice([-1, 1])
+            txtx = cent_w + x_jitter
+            txty = cent_h + y_jitter
+        except ValueError as e:
+            print "Error: test dw > img width -- ignoring..."
+            continue
 
         draw.text((txtx, txty), c_lower, fill="black", font=font)
         draw2.text((txtx, txty), c_upper, fill="black", font=font)
@@ -84,6 +114,15 @@ if __name__ == "__main__":
 
         format_string = "({}/{}) Rate font [0 - 5]: ".format(idx, len(fonts))
         rating = raw_input(format_string);
+        is_int = cast_to_int(rating)
+
+        while not is_int:
+            if rating == 'w':
+                write_ratings_to_file(fonts_ratings)
+                print "saved to file"
+            rating = raw_input(format_string)
+            is_int = cast_to_int(rating)
+
 
         fonts_ratings.append((font_path, rating))
 
