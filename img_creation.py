1b785a2f7d86a8a32cd14ad7da18a76a6bf32db9
robrao <rob.rao@arterys.com>
2018-06-01 21:38:18 -0600

    completed searches to improve bbxs

--- img_creation.py
+++ img_creation.py
@@ -32,26 +32,54 @@ def max_value_search(img, x_coord, y_coord, font_colour, width):
 
     return max_val
 
-
-def pixel_search(img, x_coord, y_coord, height, img_height, max_val):
+def pixel_search_x(img, x_coord, y_coord, height, img_height, max_val, direction):
     y_initial = y_coord
     y_final = y_coord + height
 
     while y_coord < y_final and y_coord < img_height:
         if img.getpixel((x_coord, y_coord)) < max_val:  # does not match background values
-            x_coord -= 0.1  # shift to the left
+            if direction == 'left':
+                x_coord -= 0.1  # shift to the left
+            else:
+                x_coord += 0.1  # shift to the right
             y_coord = y_initial
 
         y_coord += 0.1
 
     return x_coord
 
+def pixel_search_y(img, x_coord, y_coord, width, height, img_width, img_height, max_val):
+    y_next = y_coord
+    y_coord = y_coord - 0.1
+    y_initial = y_coord
+    x_initial = x_coord
+    y_final = y_coord + height
+    x_final = x_coord + width
+
+    while y_next < y_final and y_next < img_height:
+        while x_coord < x_final and x_coord < img_width:
+            if img.getpixel((x_coord, y_next)) < max_val:  # does not match background values
+                return y_coord
+            x_coord = x_coord + 0.01
+
+        y_coord += 0.1
+        y_next = y_coord + 0.1
+        x_coord = x_initial
+
+    return y_coord
+
 if __name__ == "__main__":
     if (os.path.isfile('font_data.csv')):
         font_data = pd.read_csv('font_data.csv', sep=',')
-        fonts = font_data[(font_data.rating != 0)].font
+        fonts = font_data[(font_data.rating != 0)].font.tolist()
 
-    # for idx in range(0, 10):
+    # Need all chars in each font, upper and lower.
+    # for idx in range(0, 50):
+    #     if idx > 25:
+    #         char = chars_lower[idx]
+    #     else:
+    #         char = chars_upper[idx]
+    #     
     for idx in range(0, 1):
         font_size = randint(30, 150)
 
@@ -64,15 +92,15 @@ if __name__ == "__main__":
 
         rnd_black = (rnd_red, rnd_blue, rnd_green, rnd_alpha)
 
-        # Randomize chars
-        # rnd = randint(0, 25)
-        rnd = 9
-        char = str(chars_lower[rnd])
+        rnd = 9 # XXX: Testing
+        char = str(chars_lower[rnd]) # XXX: Testing
+
+        # TODO: search through all fonts to find chars
+        # that intersect with bbxs only active when arg used
 
         # Randomize fonts
         rand_int = randint(0, len(fonts))
-        font_path = fonts[randint(0, len(fonts))]
-        # font_path = "/home/rrao/.fonts/Signika-Light.ttf"
+        font_path = fonts[rand_int]
         font = ImageFont.truetype(font_path, size=font_size)
 
         # Image size changes in relation to font size
@@ -86,11 +114,6 @@ if __name__ == "__main__":
         draw = ImageDraw.Draw(img, "RGBA")
         bbx = draw.textsize(char, font=font)
 
-        # per char size
-        # w, h = draw.textsize(c, font)
-        # char_size = (w, h)
-        # char_offset = font.getoffset(c)
-
         try:
             cent_w = (im_w - bbx[0]) / 2.0
             cent_h = (im_h - bbx[1]) / 2.0
@@ -102,18 +125,20 @@ if __name__ == "__main__":
             print "Error: test bbx 0 > img width -- ignoring..."
             continue
 
-        draw.text((txtx, txty), char, rnd_black, font=font)
+        # TODO: Shading not working...
+        # draw.text((txtx, txty), char, rnd_black, font=font)
+        draw.text((txtx, txty), char, 'black', font=font)
 
-        offset = [txtx, txty]
-
-        x1 = offset[0] #+ charoffset_x
-        y1 = offset[1] #+ charoffset_y
+        x1 = txtx
+        y1 = txty
+        y2 = y1 + bbx[1]
 
         max_val = max_value_search(img, x1, y1, rnd_black, bbx[0])
-        x1 = pixel_search(img, x1, y1, bbx[1], im_h, max_val)
+        x1 = pixel_search_x(img, x1, y1, bbx[1], im_h, max_val, 'left')
+        y1 = pixel_search_y(img, x1, y1, bbx[0], bbx[1], im_w, im_h, max_val)
+        x2 = pixel_search_x(img, x1 + bbx[0], y1, bbx[1], im_h, max_val, 'right')
 
-        x2 = x1 + bbx[0]
-        y2 = offset[1] + bbx[1]
+        # x2 = x1 + bbx[0]
 
         w = bbx[0]/im_w_f
         h = bbx[1]/im_h_f
