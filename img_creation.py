f6c49022aea84c7958c36d7fbe82638b15d23715
robrao <rob.rao@arterys.com>
2018-06-03 22:42:02 -0600

    fixed shading, initial bbx intersection testing

--- img_creation.py
+++ img_creation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import fnmatch
+import argparse
 import pandas as pd
 
 from random import randint, choice
@@ -68,100 +69,150 @@ def pixel_search_y(img, x_coord, y_coord, width, height, img_width, img_height, 
 
     return y_coord
 
+# TODO: don't do extra checks if intersection found along one line
+def check_bbx_for_intersection(x1, y1, x2, y2, img, font_colour):
+    intersection_found = False
+    x_current = x1
+    while x_current < x2: # check bottom horizontal line
+        if img.getpixel((x_current, y1)) == font_colour:  # does not match background values
+            intersection_found = True
+            print "intersection along x line and y1: {}".format(img.getpixel((x_current, y1)))
+            break
+        x_current = x_current + 0.1
+
+    x_current = x1
+    while x_current < x2: # check top horizontal line
+        if img.getpixel((x_current, y2)) == font_colour:  # does not match background values
+            intersection_found = True
+            print "intersection along x line and y2: {}".format(img.getpixel((x_current, y2)))
+            break
+        x_current = x_current + 0.1
+
+    y_current = y1
+    while y_current < y2: # check left vertical line
+        if img.getpixel((x1, y_current)) == font_colour:  # does not match background values
+            intersection_found = True
+            print "intersection along left vertical line: {}".format(img.getpixel((x1, y_current)))
+            break
+        y_current = y_current + 0.1
+
+    y_current = y1
+    while y_current < y2: # check right vertical line
+        if img.getpixel((x2, y_current)) == font_colour:  # does not match background values
+            intersection_found = True
+            print "intersection along right vertical line: {}".format(img.getpixel((x1, y_current)))
+            break
+        y_current = y_current + 0.1
+
+    if intersection_found:
+        img.show()
+    else:
+        print "No Intersection"
+
+def get_argparser():
+    aparser = argparse.ArgumentParser(description="create OCR data...")
+    aparser.add_argument('-cb', '--check-bbxs', action='store_true', help="check bounding box perimeter for an intersection with character, display image if intersection exists.")
+
+    return aparser
+
+
 if __name__ == "__main__":
+    parser = get_argparser();
+    args = parser.parse_args();
+
     if (os.path.isfile('font_data.csv')):
         font_data = pd.read_csv('font_data.csv', sep=',')
         fonts = font_data[(font_data.rating != 0)].font.tolist()
+        fonts = fonts[:1] # XXX: Testing
 
-    # Need all chars in each font, upper and lower.
-    # for idx in range(0, 50):
-    #     if idx > 25:
-    #         char = chars_lower[idx]
-    #     else:
-    #         char = chars_upper[idx]
-    #     
-    for idx in range(0, 1):
-        font_size = randint(30, 150)
+    for font_path in fonts:
+        # Need all chars in each font, upper and lower.
+        # for idx in range(0, 50):
+        for idx in range(0, 1):
+            idx = 9
+            if idx > 25:
+                char = chars_lower[idx]
+            else:
+                char = chars_upper[idx]
+            
+            font_size = randint(30, 150)
 
-        # Randomize shade of black
-        rnd_shade = randint(0, 90)
-        rnd_red = rnd_shade + randint(0, 2)
-        rnd_blue = rnd_shade + randint(0, 2)
-        rnd_green = rnd_shade + randint(0, 2)
-        rnd_alpha = rnd_shade + randint(0, 2)
+            # Randomize shade of black
+            rnd_shade = randint(0, 20)
+            rnd_red = rnd_shade + randint(0, 2)
+            rnd_blue = rnd_shade + randint(0, 2)
+            rnd_green = rnd_shade + randint(0, 2)
+            rnd_alpha = randint(240, 255)
 
-        rnd_black = (rnd_red, rnd_blue, rnd_green, rnd_alpha)
+            rnd_black = (rnd_red, rnd_blue, rnd_green, rnd_alpha)
 
-        rnd = 9 # XXX: Testing
-        char = str(chars_lower[rnd]) # XXX: Testing
+            # TODO: test bbx intersection on couple of fonts
+            # TODO: run on all fonts
 
-        # TODO: Shading not working...
-        # TODO: search through all fonts to find chars
-        # that intersect with bbxs only active when arg used
+            font = ImageFont.truetype(font_path, size=font_size)
 
-        # Randomize fonts
-        rand_int = randint(0, len(fonts))
-        font_path = fonts[rand_int]
-        font = ImageFont.truetype(font_path, size=font_size)
+            # Image size changes in relation to font size
+            im_w_f = font_size * 2
+            im_h_f = font_size + im_w_f
+            im_w = int(im_w_f)
+            im_h = int(im_h_f)
 
-        # Image size changes in relation to font size
-        im_w_f = font_size * 2
-        im_h_f = font_size + im_w_f
-        im_w = int(im_w_f)
-        im_h = int(im_h_f)
+            # randomize background on image
+            img = Image.new('RGBA', (im_w, im_h), 'white')
+            draw = ImageDraw.Draw(img, "RGBA")
+            bbx = draw.textsize(char, font=font)
 
-        # randomize background on image
-        img = Image.new('RGBA', (im_w, im_h), 'white')
-        draw = ImageDraw.Draw(img, "RGBA")
-        bbx = draw.textsize(char, font=font)
+            try:
+                cent_w = (im_w - bbx[0]) / 2.0
+                cent_h = (im_h - bbx[1]) / 2.0
+                x_jitter = randint(0, int((im_w - bbx[0]) * 0.25)) * choice([-1, 1])
+                y_jitter = randint(0, int((im_h - bbx[1]) * 0.25)) * choice([-1, 1])
+                txtx = cent_w + x_jitter
+                txty = cent_h + y_jitter
+            except ValueError as e:
+                print "Error: test bbx 0 > img width -- ignoring..."
+                continue
 
-        try:
-            cent_w = (im_w - bbx[0]) / 2.0
-            cent_h = (im_h - bbx[1]) / 2.0
-            x_jitter = randint(0, int((im_w - bbx[0]) * 0.25)) * choice([-1, 1])
-            y_jitter = randint(0, int((im_h - bbx[1]) * 0.25)) * choice([-1, 1])
-            txtx = cent_w + x_jitter
-            txty = cent_h + y_jitter
-        except ValueError as e:
-            print "Error: test bbx 0 > img width -- ignoring..."
-            continue
+            draw.text((txtx, txty), char, fill=rnd_black, font=font)
 
-        # draw.text((txtx, txty), char, rnd_black, font=font)
-        draw.text((txtx, txty), char, 'black', font=font)
+            x1 = txtx
+            y1 = txty
+            y2 = y1 + bbx[1]
 
-        x1 = txtx
-        y1 = txty
-        y2 = y1 + bbx[1]
+            max_val = max_value_search(img, x1, y1, rnd_black, bbx[0])
+            x1 = pixel_search_x(img, x1, y1, bbx[1], im_h, max_val, 'left')
+            y1 = pixel_search_y(img, x1, y1, bbx[0], bbx[1], im_w, im_h, max_val)
+            x2 = pixel_search_x(img, x1 + bbx[0], y1, bbx[1], im_h, max_val, 'right')
 
-        max_val = max_value_search(img, x1, y1, rnd_black, bbx[0])
-        x1 = pixel_search_x(img, x1, y1, bbx[1], im_h, max_val, 'left')
-        y1 = pixel_search_y(img, x1, y1, bbx[0], bbx[1], im_w, im_h, max_val)
-        x2 = pixel_search_x(img, x1 + bbx[0], y1, bbx[1], im_h, max_val, 'right')
+            w = bbx[0]/im_w_f
+            h = bbx[1]/im_h_f
+            cx = (x1 + 0.5 * bbx[0])/im_w_f
+            cy = (y1 + 0.5 * bbx[1])/im_h_f
 
-        w = bbx[0]/im_w_f
-        h = bbx[1]/im_h_f
-        cx = (x1 + 0.5 * bbx[0])/im_w_f
-        cy = (y1 + 0.5 * bbx[1])/im_h_f
+            draw.rectangle([x1, y1, x2, y2], outline='red')
 
-        draw.rectangle([x1, y1, x2, y2], outline='red')
+            label = "font: {} char: {} - {} {} {} {} -- colour: {}".format(font_path, idx, cx, cy, w, h, rnd_black)
+            print label
 
-        label = "font: {} char: {} - {} {} {} {}".format(font_path, rnd, cx, cy, w, h)
-        print label
+            if (args.check_bbxs):
+                check_bbx_for_intersection(x1, y1, x2, y2, img, rnd_black)
+            else:
+                img.show()
 
-        # Blur image
-        # rnd_blur = rnd_uniform(0.0, 20.0) * im_h_f/1000
-        # img = img.filter(ImageFilter.GaussianBlur(rnd_blur))
+            # Blur image
+            # rnd_blur = rnd_uniform(0.0, 20.0) * im_h_f/1000
+            # img = img.filter(ImageFilter.GaussianBlur(rnd_blur))
 
-        # Noise Image
-        # npim = np.asarray(img)
-        # noise = random_noise(
-                # npim,
-                # 'speckle',
-                # mean=rnd_uniform(0, 2),
-                # var=rnd_uniform(0.01, 0.1)
-        # )
-        # res = npim * noise
-        # img = Image.fromarray(np.uint8(res))
+            # Noise Image
+            # npim = np.asarray(img)
+            # noise = random_noise(
+                    # npim,
+                    # 'speckle',
+                    # mean=rnd_uniform(0, 2),
+                    # var=rnd_uniform(0.01, 0.1)
+            # )
+            # res = npim * noise
+            # img = Image.fromarray(np.uint8(res))
 
-        img.show()
-        # img.save("imgname_{}.jpg".format(idx), "JPEG", dpi=(600, 600))
+            # img.show()
+            # img.save("imgname_{}.jpg".format(idx), "JPEG", dpi=(600, 600))
