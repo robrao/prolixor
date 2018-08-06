ea5a2b801323f01d1fed933b4a54107e5f516e20
robrao <rob.rao@arterys.com>
2018-08-06 08:28:11 -0600

    remove comment on import

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -18,7 +18,7 @@ import pandas as pd
 import tensorflow as tf
 
 from PIL import Image
-# from object_detection.utils import dataset_util
+from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
 flags = tf.app.flags
